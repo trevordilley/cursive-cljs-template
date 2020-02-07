9c56066eabb9ed415414447a0d219f84aad49edd
trevordilley <trevor@twentyideas.com>
2020-02-06 22:23:51 -0800

    Initial commit of template

---
+++ src/app/core.cljs
@@ -0,0 +1,30 @@
+(ns app.core
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+;; to start repl do:
+;; npm run watch in terminal
+;; Execute the run configuration
+;; In repl terminal type: (shadow.cljs.devtools.api/repl :app)
+;; Make sure you've opened your app in the browser for the repl to work.
+
+;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:text "Hello world!"}))
+
+(defn hello-world []
+  [:div
+   [:h1 (:text @app-state)]
+   [:h3 "Edit this and watch it change!"]])
+
+(defn start []
+  (reagent/render-component [hello-world]
+                            (. js/document (getElementById "app"))))
+(defn ^:export init []
+  ;; init is called ONCE when the page loads
+  ;; this is called in the index.html and must be exported
+  ;; so it is available even in :advanced release builds
+  (start))
+
+(defn stop []
+  ;; stop is called before any code is reloaded
+  ;; this is controlled by :before-load in the config
+  (js/console.log "stop"))
